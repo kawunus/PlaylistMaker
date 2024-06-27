41cf6f98c288f19c244bb86729d1eee55e56920a
arbuz <73398457+Markovtsev1@users.noreply.github.com>
2024-06-27 20:04:55 +0300

    добавил темную тему, добавил взаимодействия с кнопками в настройках

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,5 +1,7 @@
 package com.example.playlistmaker
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
@@ -10,11 +12,44 @@ class SettingsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_settings)
 
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
-
+        val shareView =
+            findViewById<com.google.android.material.textview.MaterialTextView>(R.id.shareView)
+        val supportView =
+            findViewById<com.google.android.material.textview.MaterialTextView>(R.id.supportView)
+        val agreementView =
+            findViewById<com.google.android.material.textview.MaterialTextView>(R.id.agreementView)
         toolbar.setNavigationOnClickListener {
             finish()
         }
 
+        shareView.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SEND)
+            val message = "https://practicum.yandex.ru/android-developer/"
+            intent.putExtra(Intent.EXTRA_TEXT, message)
+            intent.type = "text/plain"
+            val chooser = Intent.createChooser(intent, "Поделится приложением через:")
+            startActivity(chooser)
+        }
+
+        supportView.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SENDTO)
+            val message = "Спасибо разработчикам и разработчицам за крутое приложение!"
+            val title = "Сообщение разработчикам и разработчицам приложения Playlist Maker"
+            intent.data = Uri.parse("mailto:")
+            intent.putExtra(Intent.EXTRA_SUBJECT, title)
+            intent.putExtra(Intent.EXTRA_TEXT, message)
+            intent.putExtra(Intent.EXTRA_EMAIL, arrayOf("markovtsev.stas@yandex.by"))
+
+            startActivity(intent)
+        }
+
+
+        agreementView.setOnClickListener {
+            val url = Uri.parse("https://yandex.ru/legal/practicum_offer/")
+            val intent = Intent(Intent.ACTION_VIEW, url)
+
+            startActivity(intent)
+        }
     }
 
 
