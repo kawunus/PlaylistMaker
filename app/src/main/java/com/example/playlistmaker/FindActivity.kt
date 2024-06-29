9a66bbbf49c367a68184f53cd44550806c5b7ef0
arbuz <73398457+Markovtsev1@users.noreply.github.com>
2024-06-29 22:20:43 +0300

    добавил кнопку сброса в activity_find.xml. Реализовал ее работу

--- app/src/main/java/com/example/playlistmaker/FindActivity.kt
+++ app/src/main/java/com/example/playlistmaker/FindActivity.kt
@@ -1,11 +1,52 @@
 package com.example.playlistmaker
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.Toolbar
+import androidx.appcompat.app.AppCompatActivity
 
 class FindActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_find)
+        val clearButton = findViewById<ImageView>(R.id.clearIcon)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        toolbar.setNavigationOnClickListener {
+            finish()
+        }
+
+
+        val editText = findViewById<EditText>(R.id.editText)
+        val editTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                clearButton.visibility = clearButtonVisibility(s)
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+
+        }
+        editText.addTextChangedListener(editTextWatcher)
+        clearButton.setOnClickListener{
+            editText.text.clear()
+        }
+    }
+    private fun clearButtonVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
     }
 }
