bca790b501e9283ca216117c01166c807d2f9759
stanislau <s.markovtsev@gmail.com>
2024-11-04 02:29:53 +0300

    Добавил TabLayoutAdapter в LibraryActivity

--- app/src/main/java/com/example/playlistmaker/presentation/library/activity/LibraryActivity.kt
+++ app/src/main/java/com/example/playlistmaker/presentation/library/activity/LibraryActivity.kt
@@ -3,10 +3,27 @@ package com.example.playlistmaker.presentation.library.activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.playlistmaker.R
+import com.example.playlistmaker.databinding.ActivityLibraryBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class LibraryActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLibraryBinding
+    private lateinit var tabMediator: TabLayoutMediator
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_library)
+        binding = ActivityLibraryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.viewPager.adapter =
+            LibraryViewPagerAdapter(fragmentManager = supportFragmentManager, lifecycle = lifecycle)
+
+        tabMediator = TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
+            when (position) {
+                0 -> tab.text = getString(R.string.favorites)
+                1 -> tab.text = getString(R.string.playlists)
+            }
+        }
     }
 }
