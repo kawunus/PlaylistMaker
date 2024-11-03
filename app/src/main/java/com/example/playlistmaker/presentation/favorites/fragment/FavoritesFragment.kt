804d1bd34fa004e2c33ffe0260857692ea4c6e97
stanislau <s.markovtsev@gmail.com>
2024-11-04 02:10:46 +0300

    Добавил FavoritesFragment

---
+++ app/src/main/java/com/example/playlistmaker/presentation/favorites/fragment/FavoritesFragment.kt
@@ -0,0 +1,26 @@
+package com.example.playlistmaker.presentation.favorites.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.playlistmaker.databinding.FragmentFavoritesBinding
+
+class FavoritesFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = FavoritesFragment()
+    }
+
+    private lateinit var binding: FragmentFavoritesBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentFavoritesBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
