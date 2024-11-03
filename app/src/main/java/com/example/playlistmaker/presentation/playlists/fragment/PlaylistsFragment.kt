4fbeb541633decfebe15038f83e55d220290fa37
stanislau <s.markovtsev@gmail.com>
2024-11-04 02:16:47 +0300

    Добавил PlaylistsFragment

---
+++ app/src/main/java/com/example/playlistmaker/presentation/playlists/fragment/PlaylistsFragment.kt
@@ -0,0 +1,26 @@
+package com.example.playlistmaker.presentation.playlists.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.playlistmaker.databinding.FragmentPlaylistsBinding
+
+class PlaylistsFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = PlaylistsFragment()
+    }
+
+    private lateinit var binding: FragmentPlaylistsBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentPlaylistsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
