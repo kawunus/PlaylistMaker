1d7487f4b0b57f67e0a55505882f46b0566c50fc
stanislau <s.markovtsev@gmail.com>
2024-11-05 18:22:04 +0300

    Доделал FavoritesFragment-заглушку

--- app/src/main/java/com/example/playlistmaker/presentation/favorites/fragment/FavoritesFragment.kt
+++ app/src/main/java/com/example/playlistmaker/presentation/favorites/fragment/FavoritesFragment.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.playlistmaker.databinding.FragmentFavoritesBinding
+import com.example.playlistmaker.presentation.favorites.view_model.FavoritesViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class FavoritesFragment : Fragment() {
 
@@ -14,6 +16,8 @@ class FavoritesFragment : Fragment() {
     }
 
     private lateinit var binding: FragmentFavoritesBinding
+
+    private val viewModel: FavoritesViewModel by viewModel()
 
     override fun onCreateView(
         inflater: LayoutInflater,
