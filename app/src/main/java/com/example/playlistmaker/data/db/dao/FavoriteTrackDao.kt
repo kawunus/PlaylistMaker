0f041b1946e3ace555f0f79664ecd49ab89c4559
stanislau <s.markovtsev@gmail.com>
2025-01-05 23:06:47 +0300

    поменял запрос на удаление, т.к. в контексте приложения, аннотация @Delete не поможет из-за поля added_at

--- app/src/main/java/com/example/playlistmaker/data/db/dao/FavoriteTrackDao.kt
+++ app/src/main/java/com/example/playlistmaker/data/db/dao/FavoriteTrackDao.kt
@@ -1,7 +1,6 @@
 package com.example.playlistmaker.data.db.dao
 
 import androidx.room.Dao
-import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
@@ -13,12 +12,12 @@ interface FavoriteTrackDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun addTrackToFavorites(track: FavoriteTrackEntity)
 
-    @Delete
-    suspend fun deleteTrackFromFavorites(track: FavoriteTrackEntity)
+    @Query("DELETE FROM favorite_tracks WHERE id = :trackId")
+    suspend fun deleteTrackById(trackId: Long)
 
     @Query("SELECT * FROM favorite_tracks ORDER BY added_at ASC")
     suspend fun getFavoritesTracks(): List<FavoriteTrackEntity>
 
     @Query("SELECT EXISTS(SELECT 1 FROM favorite_tracks WHERE id = :trackId)")
-    fun isTrackInFavorites(trackId: Long): Boolean
+    suspend fun isTrackInFavorites(trackId: Long): Boolean
 }
