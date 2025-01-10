72a683ed67bd5bbcf96a23c884365070c66884e0
stanislau <s.markovtsev@gmail.com>
2025-01-10 21:50:41 +0300

    Сделал верстку для экрана нового плейлиста

--- app/src/main/java/com/example/playlistmaker/presentation/new_playlist/ui/fragment/NewPlaylistFragment.kt
+++ app/src/main/java/com/example/playlistmaker/presentation/new_playlist/ui/fragment/NewPlaylistFragment.kt
@@ -1,10 +1,14 @@
 package com.example.playlistmaker.presentation.new_playlist.ui.fragment
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.example.playlistmaker.R
 import com.example.playlistmaker.databinding.FragmentNewPlaylistBinding
 
 class NewPlaylistFragment : Fragment() {
@@ -19,5 +23,32 @@ class NewPlaylistFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) = with(binding) {
+        super.onViewCreated(view, savedInstanceState)
+
+        nameEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun afterTextChanged(content: Editable?) {
+                if (!content.isNullOrEmpty()) {
+                    createButton.backgroundTintList = ContextCompat.getColorStateList(
+                        requireContext(),
+                        R.color.buttonCreateColorAble
+                    )
+                } else {
+                    createButton.backgroundTintList = ContextCompat.getColorStateList(
+                        requireContext(),
+                        R.color.buttonCreateColorDisable
+                    )
+                }
+            }
+
+        })
     }
 }
