5694749fa25a7f98ef5728d983b2f651ec6d66a9
stanislau <s.markovtsev@gmail.com>
2025-02-14 23:50:45 +0300

    GH-17: SettingsFragment теперь наследуется от BaseFragment

--- app/src/main/java/com/kawunus/playlistmaker/presentation/settings/ui/fragment/SettingsFragment.kt
+++ app/src/main/java/com/kawunus/playlistmaker/presentation/settings/ui/fragment/SettingsFragment.kt
@@ -1,54 +1,41 @@
 package com.kawunus.playlistmaker.presentation.settings.ui.fragment
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import com.kawunus.playlistmaker.App
+import com.kawunus.playlistmaker.core.ui.BaseFragment
 import com.kawunus.playlistmaker.databinding.FragmentSettingsBinding
 import com.kawunus.playlistmaker.presentation.settings.view_model.SettingsViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class SettingsFragment : Fragment() {
+class SettingsFragment :
+    BaseFragment<FragmentSettingsBinding, SettingsViewModel>(FragmentSettingsBinding::inflate) {
 
-    private lateinit var binding: FragmentSettingsBinding
+    override val viewModel: SettingsViewModel by viewModel()
 
-    private val viewModel: SettingsViewModel by viewModel()
+    override fun initViews() = with(binding) {
+        themeSwitcher.isChecked
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        binding = FragmentSettingsBinding.inflate(inflater, container, false)
-        return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        viewModel.observeTheme().observe(viewLifecycleOwner) { isNight ->
-            binding.themeSwitcher.isChecked = isNight
-            (requireActivity().applicationContext as App).switchTheme(isNight)
-        }
-        binding.themeSwitcher.isChecked
-
-        binding.shareView.setOnClickListener {
+        shareView.setOnClickListener {
             viewModel.shareApp()
         }
 
-        binding.supportView.setOnClickListener {
+        supportView.setOnClickListener {
             viewModel.openSupport()
         }
 
 
-        binding.agreementView.setOnClickListener {
+        agreementView.setOnClickListener {
             viewModel.openTerms()
         }
 
-        binding.themeSwitcher.setOnCheckedChangeListener { _, checked ->
+        themeSwitcher.setOnCheckedChangeListener { _, checked ->
             viewModel.switchTheme(checked)
+        }
+    }
+
+    override fun subscribe() = with(binding) {
+        viewModel.observeTheme().observe(viewLifecycleOwner) { isNight ->
+            themeSwitcher.isChecked = isNight
+            (requireActivity().applicationContext as App).switchTheme(isNight)
         }
     }
 }
