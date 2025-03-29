8e5c897d59ac0f86fd4b8ff70b32866d7dc0ee31
kawunus <s.markovtsev@gmail.com>
2025-03-30 00:03:23 +0300

    Пофиксил баг связанный с крашем приложения при попытке отправки ссылки на приложение

--- app/src/main/java/com/kawunus/playlistmaker/data/sharing/ExternalNavigatorIml.kt
+++ app/src/main/java/com/kawunus/playlistmaker/data/sharing/ExternalNavigatorIml.kt
@@ -2,7 +2,7 @@ package com.kawunus.playlistmaker.data.sharing
 
 import android.content.Context
 import android.content.Intent
-import android.net.Uri
+import androidx.core.net.toUri
 import com.kawunus.playlistmaker.R
 import com.kawunus.playlistmaker.data.dto.EmailData
 import com.kawunus.playlistmaker.domain.api.sharing.ExternalNavigator
@@ -11,27 +11,43 @@ import com.kawunus.playlistmaker.utils.converter.WordConverter
 
 class ExternalNavigatorIml(private val context: Context) : ExternalNavigator {
     override fun shareLink(link: String) {
-        val intent = Intent(Intent.ACTION_SEND)
-        intent.putExtra(Intent.EXTRA_TEXT, link)
-        intent.type = "text/plain"
-        val chooser = Intent.createChooser(intent, context.getString(R.string.share_title))
-        context.startActivity(chooser)
+        val intent = Intent(Intent.ACTION_SEND).apply {
+            putExtra(Intent.EXTRA_TEXT, link)
+            type = "text/plain"
+            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        }
+        context.startActivity(intent)
     }
 
     override fun openLink(link: String) {
-        val url = Uri.parse(link)
+        val url = link.toUri()
         val intent = Intent(Intent.ACTION_VIEW, url)
-
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
         context.startActivity(intent)
     }
 
     override fun openEmail(emailData: EmailData) {
-        val intent = Intent(Intent.ACTION_SENDTO)
-        intent.data = Uri.parse("mailto:")
-        intent.putExtra(Intent.EXTRA_SUBJECT, emailData.title)
-        intent.putExtra(Intent.EXTRA_TEXT, emailData.message)
-        intent.putExtra(Intent.EXTRA_EMAIL, arrayOf(emailData.email))
+        val intent = Intent(Intent.ACTION_SENDTO).apply {
+            data = "mailto:".toUri()
+            putExtra(Intent.EXTRA_SUBJECT, emailData.title)
+            putExtra(Intent.EXTRA_TEXT, emailData.message)
+            putExtra(Intent.EXTRA_EMAIL, arrayOf(emailData.email))
+            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        }
+        context.startActivity(intent)
+    }
 
+    override fun sharePlaylist(
+        trackList: List<Track>,
+        playlistName: String,
+        playlistDescription: String?
+    ) {
+        val message = getPlaylistMessage(trackList, playlistName, playlistDescription)
+        val intent = Intent(Intent.ACTION_SEND).apply {
+            putExtra(Intent.EXTRA_TEXT, message)
+            type = "text/plain"
+            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        }
         context.startActivity(intent)
     }
 
@@ -44,21 +60,6 @@ class ExternalNavigatorIml(private val context: Context) : ExternalNavigator {
     )
 
     override fun getTermsLink(): String = context.getString(R.string.link_to_agreement)
-
-    override fun sharePlaylist(
-        trackList: List<Track>,
-        playlistName: String,
-        playlistDescription: String?
-    ) {
-        val message = getPlaylistMessage(trackList, playlistName, playlistDescription)
-
-        val intent = Intent(Intent.ACTION_SEND)
-        intent.putExtra(Intent.EXTRA_TEXT, message)
-        intent.type = "text/plain"
-        val chooser = Intent.createChooser(intent, context.getString(R.string.share_playlist_title))
-        chooser.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-        context.startActivity(chooser)
-    }
 
     private fun getPlaylistMessage(
         trackList: List<Track>,
